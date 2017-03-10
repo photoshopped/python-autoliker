158641092ff708a046783a3ad54a62749b3b1ce7
Patrick Carroll <Patrick Carroll>
2017-03-09 17:05:57 -0800

    Rewrite of program. Fully functional, and with output

--- facebookautoliker.py
+++ facebookautoliker.py
@@ -1,27 +1,34 @@
 import pyautogui
 import time
+import sys
 
 def autoLike(max_likes):
 	num_likes = 0
+	unliked_button = None
+	clickx = clicky = 0
 	while (num_likes < max_likes):
+		start_time = time.time()
 		unliked_button = pyautogui.locateOnScreen('unliked_button.png')
-		print(unliked_button)
+		elapsed_time = time.time() - start_time
+
+		sys.stdout.write('Time to print: %s\n' % elapsed_time)
+		sys.stdout.write(str(unliked_button).join(' \n'))
+		sys.stdout.flush()
+
+		time.sleep(1)
+		
 		if (unliked_button == None):
 			pyautogui.press('pgdn')
+			time.sleep(1)
 		else:
 			clickx, clicky = pyautogui.center(unliked_button)
-			if (clickx>200) and (clickx<600):
-				pyautogui.click(x=clickx, y=clicky)
-				num_likes += 1
-	print(unliked_button)
-	print(clickx, clicky)
-	time.sleep(1)
+			pyautogui.click(x=clickx, y=clicky)
+			pyautogui.moverel(-50, 0)
+			num_likes += 1
+			time.sleep(1)
 
 def alt_tab():
-	time.sleep(1)
-	pyautogui.click(x=460, y=750)
-	time.sleep(1)
-	pyautogui.hotkey('ctrl', '1')
+	pyautogui.click(420, 740)
 
 alt_tab()
-autoLike(10)
+autoLike(5)
