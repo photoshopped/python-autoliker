3b8acfa39deaef5ef2b125b1a2f41947a4837a34
Patrick Carroll <Patrick Carroll>
2017-04-13 13:31:24 -0700

    Window has frame for an appropriate size. There are three autolike buttons of five, ten, and fifteen

--- facebookautoliker.py
+++ facebookautoliker.py
@@ -8,13 +8,30 @@ class Application(tk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.pack()
-		self.create_widgets()
 
-	def create_widgets(self):
-		self.auto_like_five = tk.Button(self)
+		self.frame = tk.Frame(master, width=480, height=620)
+		self.frame.pack()
+
+		self.create_fixed_like_buttons()
+		self.create_custom_like_button()
+
+	def create_fixed_like_buttons(self):
+		self.auto_like_five = tk.Button(self, fg='green')
 		self.auto_like_five["text"] = "Press This\nTo Auto Like Five Times"
-		self.auto_like_five["command"] = self.autoLike(5)
-		self.auto_like_five.pack(side="bottom")
+		self.auto_like_five["command"] = lambda: (self.autoLike(5))
+		self.auto_like_five.pack(side="top")
+		self.auto_like_ten = tk.Button(self, fg='yellow')
+		self.auto_like_ten["text"] = "Press This\nTo Auto Like Ten Times"
+		self.auto_like_ten["command"] = lambda: (self.autoLike(10))
+		self.auto_like_ten.pack(side="top")
+		self.auto_like_fifteen = tk.Button(self, fg='red')
+		self.auto_like_fifteen["text"] = "Press This\nTo Auto Like Fifteen Times"
+		self.auto_like_fifteen["command"] = lambda: (self.autoLike(15))
+		self.auto_like_fifteen.pack(side="top")
+
+	def create_custom_like_button(self):
+		pass
+
 
 	def autoLike(self, max_likes):
 		self.alt_tab()
@@ -47,5 +64,6 @@ class Application(tk.Frame):
 		pyautogui.hotkey('ctrl', '1')
 
 root = tk.Tk()
+root.title = "Facebook Liking Macro"
 app = Application(master=root)
 app.mainloop()
