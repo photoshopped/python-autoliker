a13af55c4993e31357a066cc7447bb0aae88520a
Patrick Carroll <Patrick Carroll>
2017-04-14 21:32:53 -0700

    GUI is fully functional

--- facebookautoliker.py
+++ facebookautoliker.py
@@ -10,7 +10,7 @@ class Application(tk.Frame):
 		self.pack()
 
 		self.create_fixed_like_buttons()
-		self.boundary_frame = tk.Frame(self, width=480, height=270)
+		self.boundary_frame = tk.Frame(self, width=480, height=20)
 		self.boundary_frame.pack()
 		self.create_status_bar()
 		self.create_custom_like_button()
@@ -57,6 +57,16 @@ class Application(tk.Frame):
 
 		self.location_found_label = tk.Label(self.status_bar_frame, text="Placeholder")
 		self.location_found_label.pack()
+		
+		self.label_one = tk.Label(self.status_bar_frame)
+		self.label_one.pack()
+		self.label_one["text"] = "Time since last screen capture: {}".format("N/A")
+		self.label_two = tk.Label(self.status_bar_frame)
+		self.label_two.pack()
+		self.label_two["text"] = "Button Coordinates: {}".format("N/A")
+		self.label_three = tk.Label(self.status_bar_frame)
+		self.label_three.pack()
+		self.label_three["text"] = "Total Time Elapsed: {}".format("N/A")
 
 		self.percent_label_var = tk.IntVar()
 		self.percent_label_var.set(0)
@@ -67,7 +77,7 @@ class Application(tk.Frame):
 		self.canvas.pack(side="left")
 
 		self.red_rectangle = self.canvas.create_rectangle(40, 0, 440, 30, fill="gray")
-		self.green_rectangle = self.canvas.create_rectangle(40, 0, 440, 30, fill="gray")
+		self.green_rectangle = self.canvas.create_rectangle(40, 0, 40, 30, fill="green")
 		
 		self.num_likes_var = tk.IntVar()
 		self.max_likes_var = tk.IntVar()
@@ -79,39 +89,45 @@ class Application(tk.Frame):
 
 	def start_status_bar(self):
 		self.canvas.itemconfig(self.red_rectangle, fill="red")
-		self.canvas.itemconfig(self.green_rectangle, fill="green")
-		self.canvas.coords(self.green_rectangle, 40, 0, 0, 30)
+		self.update()
 
 	def end_status_bar(self):
-		self.canvas.coords(self.red_rectangle(40, 0, 0, 30))
-		self.canvas.coords(self.green_rectangle(40, 0, 440, 30))
+		self.canvas.coords(self.red_rectangle, 40, 0, 0, 30)
+		self.canvas.coords(self.green_rectangle, 40, 0, 440, 30)
 
 	def clean_status_bar(self):
+		#Maybe I'll do a clear button in the future
 		pass
 
+	def update_status_text(self, a, b, c):
+		self.label_one["text"] = "Time since last screen capture: {} seconds ".format(a)
+		self.label_two["text"] = "Button Coordinates: {}".format(b)
+		self.label_three["text"] = "Total Time Elapsed: {} seconds".format(int(c))
+		self.update()
+
 	def update_status_bar(self, num_likes, max_likes):
-		self.percent_label_var.set(int(num_likes * 100 / max_likes))
-		self.num_likes_var.set(num_likes)#this can just be ++ as well but I'm lazy right now lol
-		self.max_likes_var.set(max_likes)#can optimize here
+		self.percent_label["text"] = "{}%".format(int(num_likes * 100 / max_likes))
+		self.progress_label["text"] = "{}/{} Likes".format(num_likes, max_likes)
+
+
 		x = int(num_likes / max_likes * 400)
-		self.canvas.coords(self.green_rectangle, 40, 0, x, 30)
-		self.canvas.coords(self.red_rectangle, 40, 0, 400 - x, 30)
+		self.canvas.coords(self.red_rectangle, 40 + x, 0, 440, 30)
+		self.canvas.coords(self.green_rectangle, 40, 0, 40 + x, 30)
+		self.update()
 
 	def autoLike(self, max_likes):
-		#self.alt_tab()
+		self.alt_tab()
 		num_likes = 0
 		unliked_button = None
 		clickx = clicky = 0
+		total_time_tracker = time.time()
 		self.start_status_bar()
 		while (num_likes < max_likes):
 			start_time = time.time()
 			unliked_button = pyautogui.locateOnScreen('unliked_button.png')
 			elapsed_time = time.time() - start_time
 
-			sys.stdout.write('Time to print: %s\n' % elapsed_time)
-			sys.stdout.write(str(unliked_button).join(' \n'))
-			sys.stdout.write('%d likes out of %d\n\n' % (num_likes, max_likes))
-			sys.stdout.flush()
+			self.update_status_text(elapsed_time, unliked_button, start_time - total_time_tracker)
 
 			time.sleep(1)
 			
